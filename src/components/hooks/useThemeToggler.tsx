e4e11aecb94a7014b481611e4541e79d38e1d334
Navdeep Singh <navdeep.er@gmail.com>
2020-11-09 16:58:02 +0530

    WIP

--- src/components/hooks/useThemeToggler.tsx
+++ src/components/hooks/useThemeToggler.tsx
@@ -4,19 +4,19 @@ export const useThemeToggler = (): [
   string,
   () => void
 ] => {
-
   const [theme, setTheme] = useState('dark');
 
   const toggleTheme = () => {
     if (theme === 'dark') {
       // Dispatch Action
+      
     } else {
       //
     }
   }
 
   useEffect(() => {
-    //getJoke();
+    
   }, [])
 
   return [theme, toggleTheme]
