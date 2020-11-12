2d342660b1b3e1c5dd9cd28f8ad23ee865d8fd75
Navdeep Singh <navdeep.er@gmail.com>
2020-11-12 12:06:42 +0530

    Update: removed redux, hooks used

--- src/components/hooks/useThemeToggler.tsx
+++ src/components/hooks/useThemeToggler.tsx
@@ -1,23 +1,18 @@
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export const useThemeToggler = (): [
   string,
-  () => void
+  () => void  
 ] => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('dark');  
 
   const toggleTheme = () => {
     if (theme === 'dark') {
-      // Dispatch Action
-      
+      setTheme('bright');      
     } else {
-      //
+      setTheme('dark');
     }
   }
 
-  useEffect(() => {
-    
-  }, [])
-
-  return [theme, toggleTheme]
+  return [theme, toggleTheme];
 }
--- src/components/ui/day-night-switcher/index.tsx
+++ src/components/ui/day-night-switcher/index.tsx
@@ -1,19 +1,46 @@
-import React, {useRef} from "react";
+import React, {useRef, useEffect} from "react";
 import {useThemeToggler} from "../../hooks/useThemeToggler";
 
 import "./index.scss";
 
 // Getting state & action through props is first step for resuable components
-
 const DayNightSwitcher = () => {
+
   const [theme, toggleTheme] = useThemeToggler();
+
   const inputCheckbox = useRef(null);
   const parentElement = useRef(null);
+
+
+  useEffect(() => {
+    console.log('Trigger Renderer:' + theme)
+    if (theme === 'bright') {
+      parentElement.current.classList.remove('react-toggle--checked')
+      inputCheckbox.current.setAttribute('aria-checked', 'false')
+    } else {
+      parentElement.current.classList.add('react-toggle--checked')
+      inputCheckbox.current.setAttribute('aria-checked', 'true')
+    }    
+  }, [theme]);
+
+  const handleFocus = () => {
+    document.querySelector(".react-toggle").classList.add('react-toggle--focus');
+  }
+
+  const handleClick = () => {
+    document.querySelector(".react-toggle").classList.add('react-toggle--focus');
+    inputCheckbox.current.focus();
+    toggleTheme();
+  }
+
+  const handleBlur = () => {  
+    document.querySelector(".react-toggle").classList.remove('react-toggle--focus');
+  }
 
   return(
     <>
       <div className="react-toggle react-toggle--checked"
-        onClick={toggleTheme}
+        onClick={handleClick}
         ref={parentElement}
       >
         <div className="react-toggle-track">
@@ -31,6 +58,8 @@ const DayNightSwitcher = () => {
           aria-label="Dark mode toggle"
           aria-checked="false"          
           className="react-toggle-screenreader-only"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
         />
       </div>
     </>
